8bcbcd4b157c5af3d54992e4bb472b4bdd710ef6
Janice Kusuma <janicekusumaaa@gmail.com>
2025-05-04 23:17:59 +0700

    Add files via upload

---
+++ storage/framework/views/2e2ff99406b257e8161353027b1ac09d.php
@@ -0,0 +1,51 @@
+
+
+<?php $__env->startSection('title', 'Data Mahasiswa'); ?>
+
+<?php $__env->startSection('content'); ?>
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title text-center"> Data Mahasiswa 
+            <a href="<?php echo e(route('mahasiswa.create')); ?>" class="btn btn-success float-end"> Tambah </a>
+        </h3>
+        
+    </div>
+
+    <div class="card-body">
+        <?php if(session('success')): ?>
+        <div class="alert alert-success"><?php echo e(session('success')); ?></div>
+        <?php endif; ?>
+
+        <table class="table table-bordered text-center">
+            <tr>
+                <th>ID</th>
+                <th>Nama</th>
+                <th>NIM</th>
+                <th>Alamat</th>
+                <th>Action</th>
+            </tr>
+
+            <?php $__currentLoopData = $mhs; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $mahasiswa): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <tr>
+                <td><?php echo e($mahasiswa->id); ?></td>
+                <td><?php echo e($mahasiswa->nama); ?></td>
+                <td><?php echo e($mahasiswa->nim); ?></td>
+                <td><?php echo e($mahasiswa->alamat); ?></td>
+                <td>
+                    <a href="<?php echo e(route('mahasiswa.edit', $mahasiswa->id)); ?>"
+                    class="btn btn-warning"> Edit </a>
+
+                    <form action="<?php echo e(route('mahasiswa.destroy', $mahasiswa->id)); ?>"
+                    method="POST" class="d-inline">
+                    <?php echo csrf_field(); ?>
+                    <?php echo method_field('DELETE'); ?> 
+                    <button type="submit" class="btn btn-danger" onclick="return confirm('Yakin dihapus?')"> Delete </button>
+                    </form>
+                </td>
+            </tr>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </table>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH D:\PHP\htdocs\website\laravel\crud-mahasiswa\resources\views/mahasiswa/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e807d9f58c9dfebb3d9807bb943656c4.php
@@ -0,0 +1,33 @@
+
+
+<?php $__env->startSection('title', 'Edit Data'); ?>
+
+<?php $__env->startSection('content'); ?>
+    <div class="card form-container">
+        <div class="card-header">
+            <h3 class="card-title text-center"> Edit Data </h3>
+        </div>
+        <div class="card-body">
+            <form action="<?php echo e(route('mahasiswa.update', $mhs->id)); ?>" method="POST">
+                <?php echo csrf_field(); ?>
+                <?php echo method_field('PUT'); ?>
+
+                <div class="mb-3">
+                    <label for="nama" class="form-label"> Nama </label>
+                    <input type="text" class="form-control" id="nama" name="nama" value="<?php echo e($mhs->nama); ?>" required>
+                </div>
+                <div class="mb-3">
+                    <label for="nim" class="form-label"> NIM </label>
+                    <input type="text" class="form-control" id="nim" name="nim" value="<?php echo e($mhs->nim); ?>" required>
+                </div>
+                <div class="mb-3">
+                    <label for="alamat" class="form-label"> Alamat </label>
+                    <textarea class="form-control" id="alamat" name="alamat" rows="3" required><?php echo e($mhs->alamat); ?></textarea>
+                </div>
+                <button type="submit" class="btn btn-success"> Update </button>
+                <a href="<?php echo e(route('mahasiswa.index', $mhs->id)); ?>" class="btn btn-secondary"> Kembali </a>
+            </form>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH D:\PHP\htdocs\website\laravel\crud-mahasiswa\resources\views/mahasiswa/edit.blade.php ENDPATH**/ ?>
